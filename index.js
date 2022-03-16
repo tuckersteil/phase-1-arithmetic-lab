ffbec48da69eac93621f7898ca3d708c5b51b606
Tucker Steil <tuckersteil@gmail.com>
2022-03-16 13:13:18 -0700

    commit tree 87f1301f90b2837994250017f45b188f41329db7

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2 
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+let random;
+random = Math.floor(Math.random() + 1);
+
+let num3 = 6
+let num4 = 16
+let mod 
+mod = num4 % num3
+
+let max; 
+max = Math.max(1, 5, 10, 15, 20)
